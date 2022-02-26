311a6a76a201098fe61e09a45fc69c1ea2f2e6cd
Awertol <michal.helgert@seznam.cz>
2022-02-26 22:16:13 +0100

    Přidejte soubory projektu.

---
+++ IT1A_25_1_2022/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Zadejte email: ");
+string email = Console.ReadLine();
+if(email.Contains("@"))
+{
+    email = email.Trim();
+    if(email.EndsWith(".cz"))
+    {
+        Console.WriteLine("Váš email je dlouhý " + email.Length + " znaků");
+    }
+    else if(email.EndsWith(".sk"))
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Váš email je dlhý " + email.Length + " znaků");
+    }
+    else if(email.EndsWith(".com"))
+    {
+        Console.WriteLine("Your email address is " + email.Length + " characters long");
+    }
+}
+else
+{
+    Console.WriteLine("Chybně zadaný email :(");
+}
